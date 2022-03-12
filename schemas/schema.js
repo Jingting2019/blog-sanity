8ec153b91575b90ac11e0596fdb6c2624c32c65a
Jingting Liu <fengshahuang@gmail.com>
2022-03-11 17:46:55 -0800

    Remove comments.

--- schemas/schema.js
+++ schemas/schema.js
@@ -11,7 +11,6 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
     {
       name: 'author',
       type: 'document',
